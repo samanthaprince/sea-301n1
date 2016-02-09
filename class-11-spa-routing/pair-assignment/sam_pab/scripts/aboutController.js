e10a5077408ed9d19fbf6b0c275acffbe8d9bf59
samanthaprince928@gmail.com <samanthaprince928@gmail.com>
2016-02-08 21:20:04 -0800

    working controller

--- class-11-spa-routing/pair-assignment/sam_pab/scripts/aboutController.js
+++ class-11-spa-routing/pair-assignment/sam_pab/scripts/aboutController.js
@@ -1,9 +1,9 @@
 (function(module) {
   var aboutController = {};
 
-  // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
   aboutController.index = function() {
-
+    $('main > section').hide();
+    $('#about').show();
   };
 
   module.aboutController = aboutController;
